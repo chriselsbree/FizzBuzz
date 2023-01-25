dadd2953c64c91a0085b1d092d95077470cfa38c
Chris Elsbree <chrise@iconics.com>
2023-01-25 10:35:47 -0500

    R calculate

--- FizzBuzz.cs
+++ FizzBuzz.cs
@@ -6,7 +6,7 @@ public class FizzBuzz
     {
         int one = 1;
         var comma = ",";
-        int two = 1 + 1;
+        int two = one + 1;
         var appleSauce = one + comma + two + comma;
         return appleSauce+"Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
     }
