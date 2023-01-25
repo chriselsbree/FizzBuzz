4ab3114b3a878465e61135c3d5f7a994fe5e2284
Chris Elsbree <chrise@iconics.com>
2023-01-25 11:19:53 -0500

    R  calulate
    Co-Authored-By: iconics-rich <66848248+iconics-rich @users.noreply.github.com>
    Co-Authored-By: ReniKIcon17 <109752214+renikicon17@users.noreply.github.com>
    Co-Authored-By: Mellidium <17202886+mellidium@users.noreply.github.com>

--- FizzBuzz.cs
+++ FizzBuzz.cs
@@ -12,7 +12,7 @@ public class FizzBuzz
 
         }
 
-        number += "Fizz";
-        return number+ ",4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+        number += "Fizz"+comma;
+        return number+ "4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
     }
 }
