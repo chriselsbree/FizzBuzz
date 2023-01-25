499840bd08397d2089f20d3f1f612d2f9e540880
Chris Elsbree <chrise@iconics.com>
2023-01-25 10:17:54 -0500

    R separation

--- FizzBuzz.cs
+++ FizzBuzz.cs
@@ -6,6 +6,6 @@ public class FizzBuzz
     {
         var one = "1";
         var comma = ",";
-        return one+ comma + "2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+        return one+ comma + "2"+",Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
     }
 }
