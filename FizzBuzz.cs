d270a94fe632cae81e40bd2b4c94311a12b3a1a2
Chris Elsbree <chrise@iconics.com>
2023-01-25 10:41:16 -0500

    R automate

--- FizzBuzz.cs
+++ FizzBuzz.cs
@@ -7,7 +7,13 @@ public class FizzBuzz
         int one = 1;
         var comma = ",";
         int two = one + 1;
-        var appleSauce = one + comma + two + comma;
+        string number = "";
+        for (int j = 0; j < 1; j++)
+        {
+            number = one + comma;
+
+        }
+        var appleSauce = number + two + comma;
         return appleSauce+"Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
     }
 }
