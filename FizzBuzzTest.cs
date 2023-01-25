01c29c0c858e021b8804bfc341c3d264ee3fc782
Chris Elsbree <chrise@iconics.com>
2023-01-25 10:06:32 -0500

    passing test

---
+++ FizzBuzzTest.cs
@@ -0,0 +1,16 @@
+using NuGet.Frameworks;
+
+namespace FizzBuzz2
+{
+    [TestClass]
+    public class FizzBuzzTest
+    {
+        [TestMethod]
+        public void TestFizzBuzz()
+        {
+            var output = FizzBuzz.Calculate(20);
+            string expected = "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
