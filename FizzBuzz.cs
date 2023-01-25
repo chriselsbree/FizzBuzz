119fbf72522506bedb0cf022392f75c1f10b77fb
Chris Elsbree <chrise@iconics.com>
2023-01-25 10:13:54 -0500

    R separation

--- FizzBuzz.cs
+++ FizzBuzz.cs
@@ -5,6 +5,6 @@ public class FizzBuzz
     public static string Calculate(int i)
     {
         var one = "1";
-        return one+",2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+        return one+ "," + "2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
     }
 }
