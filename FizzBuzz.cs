14f082fd3dc95899090dbf03cdbe305ee30d65f0
Chris Elsbree <chrise@iconics.com>
2023-01-25 10:08:20 -0500

    separate

--- FizzBuzz.cs
+++ FizzBuzz.cs
@@ -4,6 +4,7 @@ public class FizzBuzz
 {
     public static string Calculate(int i)
     {
-        return "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+
+        return "1"+",2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
     }
 }
