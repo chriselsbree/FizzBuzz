46f678510e9c577080de189a9904e74047bc02c8
Chris Elsbree <chrise@iconics.com>
2023-01-25 10:15:39 -0500

    r Encapsulte

--- FizzBuzz.cs
+++ FizzBuzz.cs
@@ -5,6 +5,7 @@ public class FizzBuzz
     public static string Calculate(int i)
     {
         var one = "1";
-        return one+ "," + "2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+        var comma = ",";
+        return one+ comma + "2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
     }
 }
