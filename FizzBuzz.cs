095990b10cfd9871ebfa6111899153be973e1955
Chris Elsbree <chrise@iconics.com>
2023-01-25 11:26:32 -0500

    R separate
    Co-Authored-By: iconics-rich <66848248+iconics-rich @users.noreply.github.com>
    Co-Authored-By: ReniKIcon17 <109752214+renikicon17@users.noreply.github.com>
    Co-Authored-By: Mellidium <17202886+mellidium@users.noreply.github.com>
    Co-Authored-By: alexKlimov781 <123010130+alexKlimov781@users.noreply.github.com>
    Co-Authored-By: Josh Obal <46050567+milfordjosh@users.noreply.github.com>

--- FizzBuzz.cs
+++ FizzBuzz.cs
@@ -18,6 +18,6 @@ public class FizzBuzz
             }
         }
 
-        return number+  "Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+        return number +  "Buzz" + ",Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
     }
 }
