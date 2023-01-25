981ec14491936352a275b750fbde54b16e30a73c
Chris Elsbree <chrise@iconics.com>
2023-01-25 11:15:18 -0500

    R separate
    Co-Authored-By: iconics-rich <66848248+iconics-rich @users.noreply.github.com>
    Co-Authored-By: ReniKIcon17 <109752214+renikicon17@users.noreply.github.com>
    Co-Authored-By: Mellidium <17202886+mellidium@users.noreply.github.com>

--- FizzBuzz.cs
+++ FizzBuzz.cs
@@ -12,6 +12,6 @@ public class FizzBuzz
 
         }
 
-        return number+"Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+        return number+ "Fizz" + ",4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
     }
 }
