a293c4a62548dfcd3b452f79f181702b122067ac
Chris Elsbree <chrise@iconics.com>
2023-01-25 11:19:00 -0500

    R Calculate
    Co-Authored-By: iconics-rich <66848248+iconics-rich @users.noreply.github.com>
    Co-Authored-By: ReniKIcon17 <109752214+renikicon17@users.noreply.github.com>
    Co-Authored-By: Mellidium <17202886+mellidium@users.noreply.github.com>

--- FizzBuzz.cs
+++ FizzBuzz.cs
@@ -12,6 +12,7 @@ public class FizzBuzz
 
         }
 
-        return number+ "Fizz" + ",4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+        number += "Fizz";
+        return number+ ",4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
     }
 }
