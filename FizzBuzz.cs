6c39c2cdadeeb8615b26a92ea5b0128e26586c02
Chris Elsbree <chrise@iconics.com>
2023-01-25 11:31:35 -0500

    R automate
    Co-Authored-By: iconics-rich <66848248+iconics-rich @users.noreply.github.com>
    Co-Authored-By: ReniKIcon17 <109752214+renikicon17@users.noreply.github.com>
    Co-Authored-By: Mellidium <17202886+mellidium@users.noreply.github.com>
    Co-Authored-By: alexKlimov781 <123010130+alexKlimov781@users.noreply.github.com>
    Co-Authored-By: Josh Obal <46050567+milfordjosh@users.noreply.github.com>

--- FizzBuzz.cs
+++ FizzBuzz.cs
@@ -5,23 +5,23 @@ public class FizzBuzz
     public static string Calculate(int i)
     {
         var comma = ",";
-        string number = "";
-        for (int j = 1; j <= 5; j++)
+        string sequence = "";
+        for (int j = 1; j <= 6; j++)
         {
-            if (j == 3)
+            if (j == 3 || j==6 )
             {
-                number += "Fizz"+comma;
+                sequence += "Fizz"+comma;
             }
             else if (j == 5)
             {
-                number += "Buzz" + comma;
+                sequence += "Buzz" + comma;
             }
             else
             {
-                number += j + comma;
+                sequence += j + comma;
             }
         }
 
-        return number + "Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+        return sequence + "7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
     }
 }
