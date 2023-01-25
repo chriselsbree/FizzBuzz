f258eb9c8280107174e6e60c30017f2151056937
Chris Elsbree <chrise@iconics.com>
2023-01-25 11:09:21 -0500

    r Clean
    Co-Authored-By: ReniKIcon17 <109752214+renikicon17@users.noreply.github.com>
    Co-Authored-By: Mellidium <17202886+mellidium@users.noreply.github.com>

--- FizzBuzz.cs
+++ FizzBuzz.cs
@@ -4,16 +4,14 @@ public class FizzBuzz
 {
     public static string Calculate(int i)
     {
-        int one = 1;
         var comma = ",";
-        int two = one + 1;
         string number = "";
         for (int j = 1; j <= 2; j++)
         {
             number += j + comma;
 
         }
-        var appleSauce = number;
-        return appleSauce+"Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+
+        return number+"Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
     }
 }
